e091c46acfa2a3ac356f56dd57f6855b13a6e0e5
RealAnalysis <detective_boys@hotmail.com>
2017-01-27 18:00:10 +0800

    step 7 upload resume redo

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,13 @@
 class Job < ApplicationRecord
+  validates :title, presence: true
+  validates :wage_upper_bound, presence: true
+  validates :wage_lower_bound, presence: true
+  validates :wage_lower_bound, numericality: {greater_than: 0}
+
+  
   scope :published, -> { where(is_hidden: false) }
   scope :recent, -> { order('created_at DESC') }
+  has_many :resumes
 
   def publish!
     self.is_hidden = false
@@ -12,8 +19,4 @@ class Job < ApplicationRecord
     self.save
   end
 
-  validates :title, presence: true
-  validates :wage_upper_bound, presence: true
-  validates :wage_lower_bound, presence: true
-  validates :wage_lower_bound, numericality: {greater_than: 0}
 end
